7e14944e65773545b4dbb5488e72bb70b7de53db
sukrubeyy <sukru.beyy@outlook.com>
2021-01-29 01:47:08 +0300

    Create a project
    i was created a project, and added an object (That's object include sprite renderer component)

---
+++ singleTouchControl/Assets/Script/touchControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchControl : MonoBehaviour
+{
+    Touch touch;
+    Vector3 touchPosition;
+    void Update()
+    {
+        if(Input.touchCount>0)
+        {
+            touch = Input.GetTouch(0);
+            touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPosition.z = 0;
+            transform.position = touchPosition;
+        }
+     
+    }
+}
